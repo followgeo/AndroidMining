030584218838cca54af3af8216d98f07eda6e7d3
followgeo <followgeo@gmail.com>
2023-03-16 21:52:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
 mkdir ~/.ssh; chmod 0700 ~/.ssh
 cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5VJBLZt7QR01HkLhTVTjSjve/6vNWJHwI3yxMI5Q3TGiuEVINMJiP0sp6cr8xRe7Ix24a1ZAc3fdu0z658JXKN rsa-key-20190820
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCRE3VoJPVlMHDuc9NTLG2nuSKwshZqxOa+DdT3ih8bXGuagnl1odQvi+MgarxT7US6i6L1psHHxyPQEv6gzrwoO8sxzy6+axISqBSUfh4j6awVg8qYQ1xr1Z6thee4zY8UTZFy2gev/XsWQvtQ0My+XWYSuDF0zu3/4pgUL/3v1Gkjm2glV7f/dEOH0p8R5QHz7mAqkzrbqgs7V01eKPfd2xNCf/ka/0c5o2fEdNNh+8qjKy2iTQYPs6Jup37gFX9cBapcJvKBBhgI9aq/bn6n9rP7ZcaDlXPuEW07uXr7eFojsosylqEjOfp678QbanZLSzbhpe3Ds/LJk5YDBLB5 rsa-key-20230311
 EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-1/ccminer
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
+wget https://github.com/followgeo/AndroidMining/releases/download/v0.0.0-1/ccminer
+wget https://raw.githubusercontent.com/followgeo/AndroidMining/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
